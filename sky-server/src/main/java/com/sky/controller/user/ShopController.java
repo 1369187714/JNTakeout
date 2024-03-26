c6a8afca95fb2fca5a7f2e635d5fcae678412627
WENHAO <w1160730299@outlook.com>
2024-03-26 22:06:57 +0900

    [fix] user ShopController bug

--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -9,7 +9,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController("userShopController")
-@RequestMapping("/admin/user")
+@RequestMapping("/user/shop")
 @Api(tags = "店铺相关接口")
 @Slf4j
 public class ShopController {
