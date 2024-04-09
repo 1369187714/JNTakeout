84701fbc890de449b17fd54fbc580ce68d4a495c
WENHAO <w1160730299@outlook.com>
2024-04-09 20:35:33 +0900

    [ADD] OrderTask Process payment timeout orders and orders in delivery

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -41,4 +42,7 @@ public interface OrderMapper {
 
     @Select("select count(*) from orders where status = #{status};")
     Integer countStatus(Integer status );
+
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrdertimeLT(Integer status, LocalDateTime orderTime);
 }
